b4808cf9a34ee56ffb109eef11032c031774a446
chlastj0305 <chlalstjcms0305@gmail.com>
2025-09-15 11:40:06 +0900

    모듈 사용

---
+++ advance/oper.py
@@ -0,0 +1,18 @@
+# 9월 15일 수업내용
+
+# 변수
+field1 = 10
+field2 =  'String'
+
+# 함수
+def sum(a,b):
+    return a + b
+
+def minus(a,b):
+    return a - b
+
+def multiply(a,b):
+    return a*b
+
+def devision(a,b):
+    return a/b
