ba35115110f7a1cd703a7b29a4709569cc5dfcbc
chlastj0305 <chlalstjcms0305@gmail.com>
2025-09-16 15:39:28 +0900

    상세한 에러 내용

--- advance/11_handling.py
+++ advance/11_handling.py
@@ -13,7 +13,7 @@ try:
         idx += 1
 except ValueError as e:
     print(e) # 예외에 대한 대략적인 정보 출력
-    traceback.print_exc() # 상세한 예외 정보를 보여줌
+    traceback.print_exc() # 상세한 예외 정보를 보여줌 (개발자에게 필요한 정보)
     print('더 이상 3을 찾을 수 없습니다.')
 finally:
     print('======끝======')
